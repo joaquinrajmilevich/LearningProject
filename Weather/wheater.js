8b88ad7804cd069c5f70cf1e91709a3e8a62ffc0
Joaquin Rajmilevich <joaquinrajmilevich@gmail.com>
2017-11-05 20:20:01 -0300

    Update wheater.js

--- Weather/wheater.js
+++ Weather/wheater.js
@@ -1,18 +1,21 @@
 let api = "https://fcc-weather-api.glitch.me/api/current?";
 let lat, lon;
 let place = document.getElementById("place");
-$(document).ready(function(){
-  navigator.geolocation.getCurrentPosition(function (position) {
-    let lat = "lat=" + position.coords.latitude;
-    let lon = "&lon=" + position.coords.longitude;
-    getPos(lat, lon);
-  });
-});
 
 function getPos(lat, lon) {
   api = api + lat + lon;
 }
 
-$.getJSON(api, function(json) {
-  console.log(json);
-})
+$(document).ready(function(){
+  navigator.geolocation.getCurrentPosition(function (position) {
+    let lat = "lat=" + position.coords.latitude;
+    let lon = "&lon=" + position.coords.longitude;
+    getPos(lat, lon);
+    update(api);
+  });
+});
+function update(api) {
+  $.getJSON(api, function(json) {
+    place.textContent = json.name;
+  });
+};
