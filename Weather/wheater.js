4dce7a1c05d468cd981c959af092b4b56aa0c307
Joaquin Rajmilevich <joaquinrajmilevich@gmail.com>
2017-11-05 21:56:28 -0300

    Update wheater.js

--- Weather/wheater.js
+++ Weather/wheater.js
@@ -1,10 +1,10 @@
 let api = "https://fcc-weather-api.glitch.me/api/current?";
 let lat, lon;
-let place = document.getElementById("place");
-
+let change = document.querySelector("button");
 function getPos(lat, lon) {
   api = api + lat + lon;
 }
+let unit = "celsius";
 
 $(document).ready(function(){
   navigator.geolocation.getCurrentPosition(function (position) {
@@ -16,6 +16,25 @@ $(document).ready(function(){
 });
 function update(api) {
   $.getJSON(api, function(json) {
-    place.textContent = json.name;
+    $("#place").html(json.name);
+    $("#status").html(json.weather[0].main);
+    $("#icon").attr("src", json.weather[0].icon);
+    temperature(json.main.temp);
   });
 };
+function temperature(t) {
+  $("#temp").html(t + " °C");
+  change.addEventListener("click", function () {
+    if (unit == "celsius") {
+      t = Math.round(t * 9 / 5 + 32);
+      $("#temp").html(t + " °F");
+      unit = "fahrenheit";
+      change.textContent = "Fahrenheit to Celsius"
+    } else {
+      t = Math.round((t - 32) * 5 / 9);
+      $("#temp").html(t + " °C");
+      unit = "celsius";
+      change.textContent = "Celsius to Fahrenheit"
+    }
+  });
+}
