c1b0c2b159627481269951cd242359da57427a86
joaquinrajmilevich <joaquinrajmilevich@gmail.com>
2017-11-12 16:01:40 -0300

    Update wikiapi

--- wikiapi/wiki.js
+++ wikiapi/wiki.js
@@ -20,7 +20,7 @@ function showResults(api) {
 	let a = document.querySelectorAll("a");
 	$.getJSON(api, function(json) {
   	for (let i = 0; i < json[1].length; i++) {
-  		a[i].textContent += json[1][i];
+  		a[i].textContent = json[1][i];
   		a[i].setAttribute("href", json[3][i]);
   	}
   });
