dd530f6d327ce3862d34f474ef80c69a29563ba5
joaquinrajmilevich <joaquinrajmilevich@gmail.com>
2017-11-12 17:56:29 -0300

    Update wikiapi

--- wikiapi/wiki.js
+++ wikiapi/wiki.js
@@ -1,16 +1,16 @@
 let b = document.querySelector("button");
 let i = document.querySelector("input");
-let p = document.querySelector("p");
+let desc = document.getElementsByClassName("desc");
+let title = document.getElementsByClassName("title");
 i.addEventListener("keypress", function (e) {
-  let key = e.which || e.keyCode;
-  if (key === 13) {
+	let key = e.which || e.keyCode;
+	if (key === 13) {
   	setApi();
   }
 });
 
 function setApi() {
 	let api = "https://en.wikipedia.org/w/api.php?action=opensearch&origin=*&search=";
-	let format = "&format=jsonfm";
   let v = document.getElementById("search").value;
   api = api + v;
   showResults(api);
@@ -20,8 +20,10 @@ function showResults(api) {
 	let a = document.querySelectorAll("a");
 	$.getJSON(api, function(json) {
   	for (let i = 0; i < json[1].length; i++) {
-  		a[i].textContent = json[1][i];
+  		title[i].textContent = json[1][i];
+  		desc[i].textContent = json[2][i];
   		a[i].setAttribute("href", json[3][i]);
+  		a[i].style.visibility = 'visible';	
   	}
   });
 }
