d65103038f7f4e6ca6ee96ad5d4c03dd18b05f23
joaquinrajmilevich <joaquinrajmilevich@gmail.com>
2017-11-20 18:03:54 -0300

    End of project

--- twitchapi/js.js
+++ twitchapi/js.js
@@ -1,15 +1,27 @@
 const channels = [
-  'ESL_SC2', 'OgamingSC2', 'cretetion',
-  'freecodecamp', 'storbeck', 'habathcx',
-  'RobotCaleb', 'noobs2ninjas',
+  'Fortnite', 'OgamingSC2', 'imaqtpie',
+  'xqcow', 'moonducktv', 'asmongold',
+  'savjz', 'camak',
 ];
-const li = document.querySelectorAll('li');
+const a = document.querySelectorAll('a');
+const h2 = document.querySelectorAll('h2');
+const p = document.querySelectorAll('p');
+const img = document.querySelectorAll('img');
 const link = 'https://wind-bow.glitch.me/twitch-api/';
+
 function url(type, name) {
   return `${link + type}/${name}?jsoncallback=?;`;
 }
-function addStuff(i) {
+function setColors(status, i) {
+  if (status === 'Playing - ') {
+    a[i].style.backgroundColor = '#15ff00';
+  } else {
+    a[i].style.backgroundColor = '#ff2600';
+  }
+}
+function setStream(i) {
   $.getJSON(url('streams', channels[i]), (json) => {
+    const name = channels[i];
     let status;
     let gamep;
     if (json.stream === null) {
@@ -19,11 +31,24 @@ function addStuff(i) {
       status = 'This channel no longer exists';
       gamep = '';
     } else {
-      status = 'Online Playing - ';
+      status = 'Playing - ';
       gamep = json.stream.game;
     }
-    li[i].textContent = `${channels[i]}:${status}${gamep}`;
+    h2[i].textContent = name.toUpperCase();
+    p[i].textContent = `${status + gamep}`;
+    setColors(status, i);
   });
+}
+function setChannel(i) {
+  $.getJSON(url('channels', channels[i]), (json) => {
+    img[i].setAttribute('src', json.logo);
+    a[i].setAttribute('href', json.url);
+  });
+}
+
+function addStuff(i) {
+  setStream(i);
+  setChannel(i);
 }
 
 for (let i = 0; i < channels.length; i += 1) {
