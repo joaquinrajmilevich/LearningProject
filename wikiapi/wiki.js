208283332343e5706a7e7eb96ab5c08c7678adae
joaquinrajmilevich <joaquinrajmilevich@gmail.com>
2017-11-20 19:02:34 -0300

    update syntax

--- wikiapi/wiki.js
+++ wikiapi/wiki.js
@@ -1,30 +1,31 @@
-let i = document.querySelector("input");
-let desc = document.getElementsByClassName("desc");
-let title = document.getElementsByClassName("title");
-let res = document.getElementById("results");
-i.addEventListener("keypress", function(e) {
-    let key = e.which || e.keyCode;
-    if (key === 13) {
-        setApi();
-        res.style.visibility = 'visible';
-        res.style.opacity = 1;
-    }
-});
-
-function setApi() {
-    let api = "https://en.wikipedia.org/w/api.php?action=opensearch&origin=*&search=";
-    let v = document.getElementById("search").value;
-    api = api + v;
-    showResults(api);
-}
+const inp = document.querySelector('input');
+const desc = document.getElementsByClassName('desc');
+const title = document.getElementsByClassName('title');
+const res = document.getElementById('results');
 
 function showResults(api) {
-    let a = document.getElementsByClassName("des");
-    $.getJSON(api, function(json) {
-        for (let i = 0; i < json[1].length; i++) {
-            title[i].textContent = json[1][i];
-            desc[i].textContent = json[2][i];
-            a[i].setAttribute("href", json[3][i]);
-        }
-    });
+  const a = document.getElementsByClassName('des');
+  $.getJSON(api, (json) => {
+    for (let i = 0; i < json[1].length; i++) {
+      title[i].textContent = json[1][i];
+      desc[i].textContent = json[2][i];
+      a[i].setAttribute('href', json[3][i]);
+    }
+  });
 }
+
+function setApi() {
+  let api =
+    'https://en.wikipedia.org/w/api.php?action=opensearch&origin=*&search=';
+  const v = document.getElementById('search').value;
+  api += v;
+  showResults(api);
+}
+inp.addEventListener('keypress', (e) => {
+  const key = e.which || e.keyCode;
+  if (key === 13) {
+    setApi();
+    res.style.visibility = 'visible';
+    res.style.opacity = 1;
+  }
+});
