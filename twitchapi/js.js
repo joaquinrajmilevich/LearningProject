aafc55a6aeea9969d061dbe12f021bb3d67cd45b
joaquinrajmilevich <joaquinrajmilevich@gmail.com>
2017-11-20 15:12:59 -0300

    update syntax to es6

--- twitchapi/js.js
+++ twitchapi/js.js
@@ -1,31 +1,32 @@
 const channels = [
-  "ESL_SC2", "OgamingSC2", "cretetion", 
-  "freecodecamp", "storbeck", "habathcx", 
-  "RobotCaleb", "noobs2ninjas"
+  'ESL_SC2', 'OgamingSC2', 'cretetion',
+  'freecodecamp', 'storbeck', 'habathcx',
+  'RobotCaleb', 'noobs2ninjas',
 ];
-let li = document.querySelectorAll("li");
-const link = "https://wind-bow.glitch.me/twitch-api/"
-
-for (let i = 0; i < channels.length; i++) {
-  addStuff(i);
-}
+const li = document.querySelectorAll('li');
+const link = 'https://wind-bow.glitch.me/twitch-api/';
 function url(type, name) {
-  return link + type + "/" + name + "?jsoncallback=?;";
+  return `${link + type}/${name}?jsoncallback=?;`;
 }
 function addStuff(i) {
-  $.getJSON(url("streams", channels[i]), function (json) {
+  $.getJSON(url('streams', channels[i]), (json) => {
     let status;
-    let game;
+    let gamep;
     if (json.stream === null) {
-      status = "Offline";
-      game = "";
+      status = 'Offline';
+      gamep = '';
     } else if (json.stream === undefined) {
-      status = "This channel no longer exists";
-      game = "";
+      status = 'This channel no longer exists';
+      gamep = '';
     } else {
-      status = "Online Playing - ";
-      game = json.stream.game;
+      status = 'Online Playing - ';
+      gamep = json.stream.game;
     }
-    li[i].textContent = channels[i] + ": " + status + game;
+    li[i].textContent = `${channels[i]}:${status}${gamep}`;
   });
 }
+
+for (let i = 0; i < channels.length; i += 1) {
+  addStuff(i);
+}
+
