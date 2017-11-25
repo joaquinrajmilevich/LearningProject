675579e1efe4b1149d5853861f7406f151e8faed
Joaquinrajmilevich <joaquinrajmilevich@gmail.com>
2017-11-25 12:21:21 -0300

    more memory

--- chat/js.js
+++ chat/js.js
@@ -4,6 +4,10 @@ const aiMem = {};
 const userMem = [];
 const button = document.querySelector('button');
 let c = false;
+let c1 = false;
+let c2 = false;
+let c3 = false;
+
 function isIn(x, val) {
   let t;
   for (let i = 0; i < x.length; i++) {
@@ -19,27 +23,30 @@ function saludos(val) {
   if (isIn(pos, val)) {
     ai.textContent = 'Hola!';
     userMem.push(val);
+    c1 = true;
   }
 }
 
 function estado(val) {
   const pos = ['como estas?', '¿como estas?', 'que tal?', '¿que tal?'];
-  if (isIn(pos, val)) {
+  if (isIn(pos, val) && c1) {
     ai.textContent = 'Bien, tu?';
     userMem.push(val);
+    c2 = true;
   }
 }
 
 function askName(val) {
   const pos = ['bien', 'regular', 'normal', 'genial', 'muy bien'];
-  if (isIn(pos, val)) {
+  if (isIn(pos, val) && c2) {
     ai.textContent = 'Como te llamas?';
     userMem.push(val);
+    c3 = true;
   }
 }
 
 function name(val) {
-  if (!isIn(userMem, val) && c === false) {
+  if (!isIn(userMem, val) && c === false && c3) {
     ai.textContent = `Un gusto ${val}, cuantos años tienes?`;
     Object.defineProperty(aiMem, 'name', {
       value: val,
@@ -50,14 +57,15 @@ function name(val) {
 }
 
 function years(val) {
-  if (!isIn(userMem, val) && !isNaN(val)) {
+  if (!isIn(userMem, val) && !isNaN(val) && c) {
     ai.textContent = 'interesante, yo tengo un dia';
     Object.defineProperty(aiMem, 'years', {
       value: val,
     });
     console.log(aiMem);
     userMem.push(val);
-  } if (!isIn(userMem, val) && !Number.isInteger(val)) {
+  }
+  if (!isIn(userMem, val) && !Number.isInteger(val) && c) {
     ai.textContent = 'Necesito un numero valido';
   }
 }
