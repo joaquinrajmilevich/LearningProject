1b37a8a2f90be61b82d8839dd875f8ae00c564f4
Joaquinrajmilevich <joaquinrajmilevich@gmail.com>
2017-12-14 19:03:39 -0300

    update

--- tictactoe/js.js
+++ tictactoe/js.js
@@ -1,8 +1,8 @@
 const a = document.querySelectorAll('.action');
-let player = 'x';
 let stop = false;
 let xPlayed = false;
-
+const huPlayer = 'x';
+const aiPlayer = 'o';
 const winCombos = [
   [0, 1, 2],
   [3, 4, 5],
@@ -13,82 +13,117 @@ const winCombos = [
   [0, 4, 8],
   [6, 4, 2],
 ];
-const boardStatus = {
-  0: '',
-  1: '',
-  2: '',
-  3: '',
-  4: '',
-  5: '',
-  6: '',
-  7: '',
-  8: '',
-};
-const acceptedValues = [''];
+const boardStatus = [0, 1, 2, 3, 4, 5, 6, 7, 8];
 
-function filter(myObject) {
-  const obj = Object.assign({}, myObject);
-  Object.keys(obj).forEach((key) => {
-    if (acceptedValues.indexOf(obj[key]) < 0) delete obj[key];
-  });
-  return obj;
-}
 
 function wins(symbol) {
   winCombos.forEach((comb) => {
-    const fEl = boardStatus[comb[0]];
-    const sEl = boardStatus[comb[1]];
-    const lEl = boardStatus[comb[2]];
     const p0 = a[comb[0]].parentElement;
     const p1 = a[comb[1]].parentElement;
     const p2 = a[comb[2]].parentElement;
-    if (fEl === symbol && sEl === symbol && lEl === symbol) {
-      console.log(`${symbol} wins`);
-      if (symbol === 'x') {
-        p0.classList.remove('xback');
-        p1.classList.remove('xback');
-        p2.classList.remove('xback');
-      } else {
-        p0.classList.remove('oback');
-        p1.classList.remove('oback');
-        p2.classList.remove('oback');
-      }
-      p0.classList.add('win');
-      p1.classList.add('win');
-      p2.classList.add('win');
-      stop = true;
+    if (symbol === 'x') {
+      p0.classList.remove('xback');
+      p1.classList.remove('xback');
+      p2.classList.remove('xback');
+    } else {
+      p0.classList.remove('oback');
+      p1.classList.remove('oback');
+      p2.classList.remove('oback');
     }
+    p0.classList.add('win');
+    p1.classList.add('win');
+    p2.classList.add('win');
+    stop = true;
   });
+}
+function winning(board, player) {
+  if (
+    (board[0] == player && board[1] == player && board[2] == player) ||
+    (board[3] == player && board[4] == player && board[5] == player) ||
+    (board[6] == player && board[7] == player && board[8] == player) ||
+    (board[0] == player && board[3] == player && board[6] == player) ||
+    (board[1] == player && board[4] == player && board[7] == player) ||
+    (board[2] == player && board[5] == player && board[8] == player) ||
+    (board[0] == player && board[4] == player && board[8] == player) ||
+    (board[2] == player && board[4] == player && board[6] == player)
+  ) {
+    return true;
+  } else {
+    return false;
+  }
+}
+function minimax(board, cplay) {
+  let emptySpots = filterIt(boardStatus);
+  if (winning(board, huPlayer)) {
+    return { score: -10 };
+  } else if (winning(board, aiPlayer)) {
+    return { score: 10 };
+  } else if (emptySpots.length === 0) {
+    return { score: 0 };
+  }
+  const moves = [];
+  for (let spot = 0; spot < emptySpots.length; spot++) {
+    let move = {};
+    move.index = board[emptySpots[spot]];
+    boardStatus[emptySpots[spot]] = cplay;
+    if (cplay === aiPlayer) {
+      let result = minimax(board, huPlayer);
+      move.score = result.score;
+    } else {
+      let result = minimax(board, aiPlayer);
+      move.score = result.score;
+    }
+    boardStatus[emptySpots[spot]] = move.index;
+
+    moves.push(move);
+  }
+  let bestMove;
+
+  if (cplay === aiPlayer) {
+    let bestScore = -10000;
+    for (let el = 0; el < moves.length; el++) {
+      if (moves[el].score > bestScore) {
+        bestScore = moves[el].score;
+        bestMove = el;
+      }
+    }
+  } else {
+    let bestScore = 10000;
+    for (let el = 0; el < moves.length; el++) {
+      if (moves[el].score < bestScore) {
+        bestScore = moves[el].score;
+        bestMove = el;
+      }
+    }
+  }
+  return moves[bestMove];
+}
+function filterIt(board) {
+  return board.filter(s => s !== 'o' && s !== 'x');
 }
 a.forEach((e) => {
   e.addEventListener('click', () => {
     if (!stop) {
       const index = Array.prototype.indexOf.call(a, e);
-      if (boardStatus[index] !== 'x' && boardStatus[index] !== 'o') {
-        boardStatus[index] = player;
-        console.log(boardStatus[index]);
-        filter(boardStatus);
-        if (player === 'x') {
-          e.classList.add('x');
-          e.parentElement.classList.add('xback');
-          wins(player);
-          player = 'o';
-          xPlayed = true;
+      if (boardStatus[index] !== huPlayer && boardStatus[index] !== aiPlayer) {
+        boardStatus[index] = huPlayer;
+        e.classList.add(huPlayer);
+        e.parentElement.classList.add('xback');
+        if (winning(boardStatus, huPlayer)) {
+          console.log('you win');
+          wins(huPlayer);
         } else {
-          e.classList.add('circle');
-          e.parentElement.classList.add('oback');
-          wins(player);
-          player = 'x';
+          const move = minimax(boardStatus, aiPlayer).index;
+          console.log(move);
+          boardStatus[move] = aiPlayer;
+          a[move].classList.add('circle');
+          a[move].parentElement.classList.add('oback');
+          if (winning(boardStatus, aiPlayer)) {
+            console.log('you lose');
+            wins(aiPlayer);
+          }
         }
       }
-    }
-    if (xPlayed) {
-      xPlayed = false;
-      const left = Object.keys(filter(boardStatus));
-      let rand = left[Math.floor(Math.random() * left.length)];
-      window.setInterval(() => {
-        a[rand].click();
-      }, 400);
     }
   });
 });
