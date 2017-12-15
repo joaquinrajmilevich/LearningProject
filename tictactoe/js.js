3f27bd1463347d07d38ed7445d6b92f211f4ac83
Joaquinrajmilevich <joaquinrajmilevich@gmail.com>
2017-12-15 15:32:57 -0300

    end project

--- tictactoe/js.js
+++ tictactoe/js.js
@@ -2,22 +2,15 @@ const a = document.querySelectorAll('.action');
 const x = document.querySelector('#x');
 const o = document.querySelector('#o');
 const popup = document.querySelector('#popup');
+const popup1 = document.querySelector('#popup1');
 const table = document.querySelector('table');
+const p = document.querySelector('p');
 let stop = false;
 let huPlayer;
 let aiPlayer;
 let c1 = 'x';
 let c2 = 'o';
-const winCombos = [
-  [0, 1, 2],
-  [3, 4, 5],
-  [6, 7, 8],
-  [0, 3, 6],
-  [1, 4, 7],
-  [2, 5, 8],
-  [0, 4, 8],
-  [6, 4, 2],
-];
+let count = 0;
 const boardStatus = [0, 1, 2, 3, 4, 5, 6, 7, 8];
 
 function filterIt(board) {
@@ -38,35 +31,25 @@ function winning(board, player) {
   }
   return false;
 }
-function wins(symbol) {
-  winCombos.forEach((comb) => {
-    const fEl = boardStatus[comb[0]];
-    const sEl = boardStatus[comb[1]];
-    const lEl = boardStatus[comb[2]];
-    const p0 = a[comb[0]].parentElement;
-    const p1 = a[comb[1]].parentElement;
-    const p2 = a[comb[2]].parentElement;
-    if (fEl === symbol && sEl === symbol && lEl === symbol) {
-      console.log(`${symbol} wins`);
-      if (winning(boardStatus, huPlayer)) {
-        p0.classList.remove('xback');
-        p1.classList.remove('xback');
-        p2.classList.remove('xback');
-        p0.classList.add('win');
-        p1.classList.add('win');
-        p2.classList.add('win');
-      } else {
-        p0.classList.remove('oback');
-        p1.classList.remove('oback');
-        p2.classList.remove('oback');
-        p0.classList.add('wino');
-        p1.classList.add('wino');
-        p2.classList.add('wino');
-      }
-
-      stop = true;
-    }
-  });
+function wins() {
+  if (winning(boardStatus, aiPlayer)) {
+    p.innerHTML = 'YOU<br>LOSE!';
+    popup1.classList.add('show');
+    table.classList.add('filter');
+    popup1.style.visibility = 'visible';
+    stop = true;
+  } else if (winning(boardStatus, huPlayer)) {
+    p.innerHTML = 'YOU <br> WIN!';
+    popup1.classList.add('show');
+    table.classList.add('filter');
+    popup1.style.visibility = 'visible';
+    stop = true;
+  } else if (count > 8) {
+    p.innerHTML = "It's a<br>TIE!";
+    popup1.classList.add('show');
+    table.classList.add('filter');
+    popup1.style.visibility = 'visible';
+  }
 }
 
 function minimax(board, cplay) {
@@ -115,7 +98,6 @@ function minimax(board, cplay) {
   return moves[bestMove];
 }
 x.addEventListener('click', () => {
-  console.log(popup);
   huPlayer = 'x';
   aiPlayer = 'o';
   popup.classList.add('hide');
@@ -123,11 +105,11 @@ x.addEventListener('click', () => {
   table.classList.add('show');
 });
 o.addEventListener('click', () => {
-  console.log(popup);
   huPlayer = 'o';
   aiPlayer = 'x';
   c1 = 'oa';
   c2 = 'xa';
+  count += 2;
   popup.classList.add('hide');
   popup.style.visibility = 'hidden';
   table.classList.add('show');
@@ -136,9 +118,13 @@ o.addEventListener('click', () => {
   a[move].classList.add(c2);
   a[move].parentElement.classList.add('oback');
 });
+
 a.forEach((e) => {
   e.addEventListener('click', () => {
+    count += 1;
     if (!stop) {
+      count += 1;
+      window.setTimeout(wins, 1000);
       const index = Array.prototype.indexOf.call(a, e);
       if (boardStatus[index] !== huPlayer && boardStatus[index] !== aiPlayer) {
         boardStatus[index] = huPlayer;
@@ -146,7 +132,7 @@ a.forEach((e) => {
         e.parentElement.classList.add('xback');
         if (winning(boardStatus, huPlayer)) {
           console.log('you win');
-          wins(huPlayer);
+          window.setTimeout(wins, 1000);
         } else {
           window.setTimeout(() => {
             const move = minimax(boardStatus, aiPlayer).index;
@@ -155,7 +141,7 @@ a.forEach((e) => {
             a[move].parentElement.classList.add('oback');
             if (winning(boardStatus, aiPlayer)) {
               console.log('you lose');
-              wins(aiPlayer);
+              window.setTimeout(wins, 1000);
             }
           }, 500);
         }
@@ -163,3 +149,5 @@ a.forEach((e) => {
     }
   });
 });
+
+popup1.addEventListener('click', () => location.reload());
