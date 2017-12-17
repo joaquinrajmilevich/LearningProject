9db974636b54e1b1a03f750f9545049c9dd0ade8
Joaquinrajmilevich <joaquinrajmilevich@gmail.com>
2017-12-17 20:55:57 -0300

    update

--- simon/script.js
+++ simon/script.js
@@ -1,1 +1,10 @@
-console.log('done');
+const colors = document.querySelectorAll('.c');
+
+colors.forEach((e) => {
+  $(e).mousedown(() => {
+    $(e).addClass('contrast');
+  });
+  $(e).mouseup(() => {
+    $(e).removeClass('contrast');
+  });
+});
